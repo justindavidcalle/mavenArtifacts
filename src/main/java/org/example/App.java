3511997ffdfab04d88b7d8a8722385655dd720d1
justindavidcalle <justincalle@outlook.de>
2024-04-03 13:02:38 +0200

    MavenArtifact

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JustinCalculator justinCalculator = new JustinCalculator();
+
+        int resultAddFunction = justinCalculator.addInt(3,2);
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        int resultSumIntArrayFunction = justinCalculator.sumIntArray(numbers);
+
+        System.out.println(resultSumIntArrayFunction);
+    }
+}
---
+++ src/main/java/org/example/JustinCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+public class JustinCalculator {
+
+    public int addInt(int x, int y){
+        int result;
+        result = x + y;
+        return result;
+    }
+
+    public int multiplyInt(int x, int y){
+        int result;
+        result = x * y;
+
+        return result;
+    }
+
+    public int sumIntArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+
+}
